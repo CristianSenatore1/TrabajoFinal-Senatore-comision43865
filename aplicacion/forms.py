454542e1972abec0d453f2f8a85f581c55b0d60c
CristianSenatore <crsenatore.cs@gmail.com>
2023-08-17 07:28:37 +0200

    Actualiizaciones

--- aplicacion/forms.py
+++ aplicacion/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Contacto, Usados 
+from .models import Contacto, Usados, Nuevos, Service, Administracion
 
 class ContactoForm(forms.ModelForm):
 
@@ -8,8 +8,21 @@ class ContactoForm(forms.ModelForm):
         fields = '__all__'
 
 class UsadoForm(forms.ModelForm):
-
     class Meta:
         model = Usados
         fields = '__all__'
     
+class NuevoForm(forms.ModelForm):
+    class Meta:
+        model = Nuevos
+        fields = '__all__'
+
+class ServiceForm(forms.ModelForm):
+    class Meta:
+        model = Service
+        fields = '__all__'
+
+class AdministracionForm(forms.ModelForm):
+    class Meta:
+        model = Administracion
+        fields = '__all__'
